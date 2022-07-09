469b2d2a055b36ab718d7bb28865c596e8f0b9c3
Faisal <md.faisal1899@yahoo.com>
2022-07-09 10:57:24 +0600

    added header llayout

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import React, { useState } from 'react';
-
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
-
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <Fragment className="App">
+      <Header />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        <Meals />
       </main>
-    </React.Fragment>
+    </Fragment>
   );
 }
 
